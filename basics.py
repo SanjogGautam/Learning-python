552a63194a50875163e20d05aec1f26e4a218684
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2025-10-05 02:02:58 +0545

    day1

---
+++ basics.py
@@ -0,0 +1,8 @@
+print("hello world")
+name = input("hello give me your name: ")
+print("hello " + name)
+#clear
+#graph of diagonal line
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,2,3,4])    
+plt.show()
