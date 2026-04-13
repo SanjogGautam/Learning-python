b31407a549c26bdd890523d4e2fdb67276033e9f
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-13 12:32:23 +0545

    sanjog

--- Tasks/Task3/Tuples/enumerate()function.py
+++ Tasks/Task3/Tuples/enumerate()function.py
@@ -4,3 +4,7 @@ x=("a","b")
 y=enumerate(x)
 print(y)#it gives the location of the enumerate object
 print(list(y))#it gives the count of the tuple x like((0,"a"),(1,"b"))
+print(list(enumerate(x,1)))#it gives the count of the tuple x starting from 1 like((1,"a"),(2,"b"))
+list1=["a","b"]
+print(list(enumerate(list1)))#it gives the count of the list list1 like((0,"a"),(1,"b"))
+print(list(enumerate(list1,1)))#it gives the count of the list list1 starting from 1 like((1,"a"),(2,"b"))
