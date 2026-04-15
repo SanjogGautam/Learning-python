3fc33f1b44ab4884016e75c69c58eacc2a5a7efa
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-15 15:29:29 +0545

    sanjog

--- Tasks/Task 6/Encapsulation/Protected property.py
+++ Tasks/Task 6/Encapsulation/Protected property.py
@@ -1,16 +1,24 @@
-#public access modifyres we can declare public using (_prefix)
-class sanjog:
-    def __init__(self,name,age):
-        self.name=name
-        self._age=age
-    @staticmethod
-    def _sum_of(a,b):
-        return a+b
-class child(sanjog):
-    def __init__(self,name,age):
-        super().__init__(name,age)
-    def access_public_method(self):
-        return _self.sum_of(10,20)
+class Sanjog:
+    def __init__(self, name, age):
+        self.name = name     # Public
+        self._age = age      # Protected (signaled by _)
 
-c1=child("Sanjog",20)
-print(c1._age)
+    @staticmethod
+    def _sum_of(a, b):       # Protected Static Method
+        return a + b
+
+class Child(Sanjog):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def access_protected_method(self, x, y):
+        # Correct syntax: self._methodname
+        return self._sum_of(x, y)
+
+c1 = Child("Sanjog", 20)
+
+# Accessing protected variable (allowed by Python, but discouraged)
+print(f"Protected Age: {c1._age}") 
+
+# Accessing the method through the child
+print(c1.access_protected_method(20, 30))
