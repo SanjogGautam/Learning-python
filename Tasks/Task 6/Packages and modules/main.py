ab4cac1e4b0d97cd6173ded1466bc5b26948271d
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-16 12:07:31 +0545

    sanjog

---
+++ Tasks/Task 6/Packages and modules/main.py
@@ -0,0 +1,6 @@
+#module is a single .py file
+#package is a folder with multiple modules and 
+import math_help
+from sanjog_package import sanjog1
+print(math_help.add(10,20))
+print(sanjog1.mul(10,2))
