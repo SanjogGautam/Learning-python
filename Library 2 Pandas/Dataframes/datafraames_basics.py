da0334e7b632b42274e747f1df8af078e993b674
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-20 16:38:07 +0545

    sanjog

---
+++ Library 2 Pandas/Dataframes/datafraames_basics.py
@@ -0,0 +1,7 @@
+#a dataframe is a tabular data structure with rows and columns similar to a excell spreadsheet
+#its in 2D
+#it is like a excell spreadsheet
+import pandas as pd
+data={
+    "Name"
+}
---
+++ Library 2 Pandas/Series/dictionary_series.py
@@ -0,0 +1,5 @@
+import pandas as pd
+dic={"Name":"Sanjog","Age":20,"Address":"Kirtipur"}
+s=pd.Series(dic)
+print(s)
+#in dictionary we don't need to specify teh index to the dictionary
