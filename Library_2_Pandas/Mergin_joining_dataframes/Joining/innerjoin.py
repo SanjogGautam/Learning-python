8ee252315b7bb61ffea74ded17778f57c9f5dc54
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-22 10:43:38 +0545

    sanjog

---
+++ Library_2_Pandas/Mergin_joining_dataframes/Joining/innerjoin.py
@@ -0,0 +1,13 @@
+import pandas as pd
+employees= pd.DataFrame({
+    "EmpID":[1,2,3,4],
+    "Name":["Sanjog","Sarin","Swagat","Susan"],
+    "DeptID":[10,20,10,30]
+})
+departments=pd.DataFrame({
+    "DeptID":[10,20,40],
+    "DeptName":["HR","Finance","IT"]
+})
+# Performing a inner join
+inner=pd.merge(employees,departments,on="DeptID",how="inner")
+print(inner)
