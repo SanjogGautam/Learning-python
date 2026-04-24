42503afb7bd1cf3cfbbc341f55a842bb4d113558
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-04-24 10:53:02 +0545

    sanjog

--- Library_3_Matplotlib/Pandas_matplot_numpy/usingallatonce.py
+++ Library_3_Matplotlib/Pandas_matplot_numpy/usingallatonce.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+df=pd.read_csv('data.csv')
+count=df["Type1"].value_counts(ascending=True)#it counts the total values of each types in type1
+print(count)#count becomes a series
+colors=["Red","blue","green"]
+plt.bar(count.index,count.values,color=colors,label="types")
+plt.title("Type count of the pokemns")
+plt.xlabel("Types of the pokemons")
+plt.xticks(rotation=30)
+plt.ylabel("Counts of Pokemons")
+plt.legend(title="Types of Pokemons",fontsize=12,shadow=True)
+plt.tight_layout()#It manages teh congestion
+plt.show()
